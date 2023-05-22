ffb141312bf057eccad1af4bf27fbf9d8b58268d
Esther <ensf1@aluno.ifal.edu.br>
2023-05-22 13:33:10 -0300

    Setup inicial do Spring

---
+++ src/test/java/br/edu/ifal/contratos/ContratosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifal.contratos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContratosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
